ff955d789f9957f74469c77bc2b9a2d3d5a0c238
marco-agizza <marco.agizza@outlook.com>
2022-12-08 18:08:07 +0100

    Now the background with the two grounds never end

--- NC2_4Head/Scenes/GameScene.swift
+++ NC2_4Head/Scenes/GameScene.swift
@@ -10,7 +10,7 @@ import GameplayKit
 
 class GameScene: SKScene {
     // MARK: Properties
-    var backgoundImage = SKSpriteNode(imageNamed: "BackgroundImage")
+    var background = SKSpriteNode()
     var leftTree = SKSpriteNode()
     var rightTree = SKSpriteNode()
     var player = SKSpriteNode(imageNamed: "Squirrel")
@@ -20,6 +20,30 @@ class GameScene: SKScene {
     var lastUpdateTime: TimeInterval = 0.0
     var dt: TimeInterval = 0.0
     
+    
+    var playableRect: CGRect {
+        let ratio: CGFloat = 0.4 // TODO: understand better how it works
+        /*switch UIScreen.main.nativeBounds.height {
+        case 2688, 1792, 2436:
+            ratio = 2.16
+        default:
+            ratio = 16/9
+        }*/
+        let playableHeight = size.width / ratio
+        let playableMargin = (size.height - playableHeight) / 2.0
+        
+        return CGRect(x: 0.0, y: playableMargin, width: size.width, height: playableHeight)
+    }
+    
+    var cameraRect: CGRect {
+        let width = playableRect.width
+        let height = playableRect.height
+        let x = cameraNode.position.x - size.width/2.0 + (size.width - width)/2.0
+        let y = cameraNode.position.y - size.height/2.0 + (size.height - height)/2.0
+        
+        return CGRect(x: x, y: y, width: width, height: height)
+    }
+        
     // MARK: System
     
     override func didMove(to view: SKView) {
@@ -48,22 +72,26 @@ extension GameScene {
     }
     
     func createBackground() {
-        backgoundImage.anchorPoint = .zero
-        backgoundImage.position = .zero
-        backgoundImage.zPosition = -1.0
-        addChild(backgoundImage)
+        for i in 0...2 {
+            background = SKSpriteNode(imageNamed: "BackgroundImage")
+            background.name = "Background"
+            background.anchorPoint = .zero
+            background.position = CGPoint(x: 0.0, y: CGFloat(i)*background.frame.height)
+            background.zPosition = -1.0
+            addChild(background)
+        }
     }
     
     func createTrees() {
         for i in 0...2 {
             leftTree = SKSpriteNode(imageNamed: "LeftTree")
-            leftTree.name = "LeftTree"
+            leftTree.name = "Tree"
             leftTree.anchorPoint = .zero
             leftTree.zPosition = 1.0
             leftTree.position = CGPoint(x: 0.0, y: CGFloat(i)*leftTree.frame.height)
             addChild(leftTree)
             rightTree = SKSpriteNode(imageNamed: "RightTree")
-            rightTree.name = "RightTree"
+            rightTree.name = "Tree"
             rightTree.anchorPoint = CGPoint(x: 1, y:  0)
             rightTree.zPosition = 1.0
             rightTree.position = CGPoint(x: frame.width, y: CGFloat(i)*rightTree.frame.height)
@@ -88,5 +116,24 @@ extension GameScene {
     func moveCamera() {
         let amountToMove = CGPoint(x: 0.0, y: cameraMovePointPerSecond * CGFloat(dt))
         cameraNode.position = CGPoint(x: cameraNode.position.x + amountToMove.x, y: cameraNode.position.y + amountToMove.y)
+        
+        //Background
+        enumerateChildNodes(withName: "Background") { (node, _) in
+            let node = node as! SKSpriteNode
+            
+            if node.position.y + node.frame.height < self.cameraRect.origin.y {
+                node.position = CGPoint(x: node.position.x, y: node.position.y + node.frame.height*3)
+            }
+        }
+        
+        //Grounds
+        enumerateChildNodes(withName: "Tree") { (node, _) in
+            let node = node as! SKSpriteNode
+            
+            if node.position.y + node.frame.height < self.cameraRect.origin.y {
+                node.position = CGPoint(x: node.position.x, y: node.position.y + node.frame.height*3)
+            }
+        }
     }
 }
+
